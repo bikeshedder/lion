ae191482e00d16e1287f8ee1cb7d4c7800811650
Michael P. Jung <michael.jung@terreon.de>
2017-10-16 17:33:27 +0200

    Copy and benchmark code from serpy adding lion to tests

---
+++ benchmarks/bm_complex.py
@@ -0,0 +1,102 @@
+from django.conf import settings
+settings.configure()
+
+import django
+django.setup()
+
+from rest_framework import serializers as rf_serializers
+from utils import write_csv
+import marshmallow
+import serpy
+import lion
+
+
+class SubRF(rf_serializers.Serializer):
+    w = rf_serializers.FloatField()
+    x = rf_serializers.SerializerMethodField()
+    y = rf_serializers.CharField()
+    z = rf_serializers.IntegerField()
+
+    def get_x(self, obj):
+        return obj.x + 10
+
+
+class ComplexRF(rf_serializers.Serializer):
+    foo = rf_serializers.ReadOnlyField()
+    bar = rf_serializers.IntegerField()
+    sub = SubRF()
+    subs = SubRF(many=True)
+
+
+class SubM(marshmallow.Schema):
+    w = marshmallow.fields.Int()
+    x = marshmallow.fields.Method('get_x')
+    y = marshmallow.fields.Str()
+    z = marshmallow.fields.Int()
+
+    def get_x(self, obj):
+        return obj.x + 10
+
+
+class CallField(marshmallow.fields.Field):
+
+    def _serialize(self, value, attr, obj):
+        return value()
+
+
+class ComplexM(marshmallow.Schema):
+    foo = marshmallow.fields.Str()
+    bar = CallField()
+    sub = marshmallow.fields.Nested(SubM)
+    subs = marshmallow.fields.Nested(SubM, many=True)
+
+
+class SubS(serpy.Serializer):
+    w = serpy.IntField()
+    x = serpy.MethodField()
+    y = serpy.StrField()
+    z = serpy.IntField()
+
+    def get_x(self, obj):
+        return obj.x + 10
+
+
+class ComplexS(serpy.Serializer):
+    foo = serpy.StrField()
+    bar = serpy.IntField(call=True)
+    sub = SubS()
+    subs = SubS(many=True)
+
+
+class SubL(lion.Mapper):
+    w = lion.IntField()
+    x = lion.Field(getter=lambda obj, name: obj.x + 10)
+    y = lion.StrField()
+    z = lion.IntField()
+
+
+class ComplexL(lion.Mapper):
+    foo = lion.StrField()
+    bar = lion.Field(getter=lambda obj, name: obj.bar())
+    sub = lion.MapperField(SubL)
+    subs = lion.ListField(SubL)
+
+
+if __name__ == '__main__':
+    data = {
+        'foo': 'bar',
+        'bar': lambda: 5,
+        'sub': {
+            'w': 1000,
+            'x': 20,
+            'y': 'hello',
+            'z': 10
+        },
+        'subs': [{
+            'w': 1000 * i,
+            'x': 20 * i,
+            'y': 'hello' * i,
+            'z': 10 * i
+        } for i in range(10)]
+    }
+    write_csv(__file__, data, ComplexRF, ComplexM().dump, ComplexS, ComplexL().drf(), 1)
---
+++ benchmarks/utils.py
@@ -0,0 +1,57 @@
+from rest_framework import serializers as rf_serializers
+import serpy
+import lion
+import time
+
+
+class Obj(object):
+    def __init__(self, **kwargs):
+        for k, v in kwargs.items():
+            if isinstance(v, dict):
+                v = Obj(**v)
+            if isinstance(v, list):
+                v = [Obj(**attrs) for attrs in v]
+            setattr(self, k, v)
+
+
+def write_csv(name, data, drf, marshmallow, serpy, lion, size):
+    repetitions = 10
+    with open('{0}.csv'.format(name), 'w+') as f:
+        f.write(',DRF,Marshmallow,serpy,lion\n')
+        for i in range(10 * size, 101 * size, 10 * size):
+            f.write(str(i * repetitions))
+            for serializer in (drf, marshmallow, serpy, lion):
+                f.write(',')
+                f.write(str(benchmark(serializer, repetitions, i, data=data)))
+            f.write('\n')
+
+
+def benchmark(serializer_fn, repetitions, num_objs=1, data=None):
+    total_objs = repetitions * num_objs
+    if not isinstance(serializer_fn, type):
+        library = 'Marshmallow'
+    elif issubclass(serializer_fn, serpy.Serializer):
+        library = 'serpy'
+    elif issubclass(serializer_fn, rf_serializers.Serializer):
+        library = 'Django Rest Framework'
+    elif issubclass(serializer_fn, lion.contrib.drf.DRFMapper):
+        library = 'Lion'
+    else:
+        raise RuntimeError("Unknown serializer: %r", serializer_fn)
+    print('Serializing {} objects using {}'.format(total_objs, library))
+
+    if data is None:
+        data = {}
+
+    objs = [Obj(**data) for i in range(num_objs)]
+    many = num_objs > 1
+    if not many:
+        objs = objs[0]
+
+    t1 = time.time()
+    for i in range(repetitions):
+        serializer_fn(objs, many=many).data
+    total_time = time.time() - t1
+    print('Total time: {}'.format(total_time))
+    print('Objs/s    : {}\n'.format(int(total_objs / total_time)))
+    return total_time
