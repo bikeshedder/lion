0a4e0348ac8d202613cedf1473b5af0534d67f95
Michael P. Jung <michael.jung@terreon.de>
2017-10-17 16:34:42 +0200

    Improve tests including timezones and various conditions

--- tests/test_fields.py
+++ tests/test_fields.py
@@ -5,39 +5,74 @@ import pytz
 
 import lion
 
-from . import objectify
+from .utils import objectify
 
 
 def test_bool_field():
     class MyMapper(lion.Mapper):
-        a = lion.BoolField()
-        b = lion.BoolField()
-        c = lion.BoolField()
-        d = lion.BoolField()
-    data = { 'a': True, 'b': False, 'c': 'truthy value', 'd': '' }
+        v = lion.BoolField()
+        v_skip_none = lion.BoolField(condition=lion.skip_none)
+        v_skip_false = lion.BoolField(condition=lion.skip_false)
+        v_skip_true = lion.BoolField(condition=lambda v: v is not None and not v)
+        v_boolify_true = lion.BoolField()
+        v_boolify_false = lion.BoolField()
+    data = {
+        'v': True,
+        'v_skip_none': None,
+        'v_skip_false': False,
+        'v_skip_true': True,
+        'v_boolify_true': 'Hi everybody!',
+        'v_boolify_false': '',
+    }
     obj = objectify(data)
     assert MyMapper().dump(obj) == {
-        'a': True,
-        'b': False,
-        'c': True,
-        'd': False
+        'v': True,
+        'v_boolify_true': True,
+        'v_boolify_false': False,
     }
 
 
 def test_int_field():
     class MyMapper(lion.Mapper):
         v = lion.IntField()
-    data = { 'v': 42 }
+        v_skip_none = lion.IntField(condition=lion.skip_none)
+        v_skip_false = lion.IntField(condition=lion.skip_false)
+        v_skip_negative = lion.IntField(condition=lambda v: v is not None and v >= 0)
+        v_intify = lion.IntField()
+    data = {
+        'v': 42,
+        'v_skip_none': None,
+        'v_skip_false': 0,
+        'v_skip_negative': -42,
+        'v_intify': '42',
+    }
     obj = objectify(data)
-    assert MyMapper().dump(obj) == data
+    assert MyMapper().dump(obj) == {
+        'v': 42,
+        'v_intify': 42,
+    }
 
 
 def test_str_field():
     class MyMapper(lion.Mapper):
         s = lion.StrField()
-    data = { 's': 'hello world' }
+        s_none = lion.StrField()
+        s_skip_none = lion.StrField(condition=lion.skip_none)
+        s_skip_empty = lion.StrField(condition=lion.skip_empty)
+        stringify = lion.StrField()
+    data = {
+        's': 'hello world',
+        's_none': None,
+        's_skip_none': None,
+        's_skip_empty': '',
+        'stringify': 42,
+    }
     obj = objectify(data)
-    assert MyMapper().dump(obj) == data
+    assert MyMapper().dump(obj) == {
+        's': 'hello world',
+        's_none': None,
+        'stringify': '42',
+    }
 
 
 def test_const_field():
@@ -55,10 +90,40 @@ def test_const_field():
 def test_uuid_field():
     class MyMapper(lion.Mapper):
         id = lion.UUIDField()
-    data = { 'id': UUID('23101702-056b-40a3-aec2-8c10942c176d') }
+        id_skip_none = lion.UUIDField(condition=lion.skip_none)
+    data = {
+        'id': UUID('23101702-056b-40a3-aec2-8c10942c176d'),
+        'id_skip_none': None
+    }
     obj = objectify(data)
     assert MyMapper().dump(obj) == {
         'id': '23101702-056b-40a3-aec2-8c10942c176d'
+    }
+
+
+def test_datetime_field():
+    class MyMapper(lion.Mapper):
+        dt_naive = lion.DateTimeField()
+        dt_berlin = lion.DateTimeField()
+        dt_naive_to_utc = lion.DateTimeField(tz=pytz.utc)
+        dt_berlin_to_utc = lion.DateTimeField(tz=pytz.utc)
+        dt_utc_to_berlin = lion.DateTimeField(tz=pytz.timezone('Europe/Berlin'))
+        dt_skip_none = lion.DateTimeField(condition=lion.skip_none)
+    data = {
+        'dt_naive': datetime.datetime(2017, 10, 17, 15, 45, 3, tzinfo=pytz.utc),
+        'dt_berlin': pytz.timezone('Europe/Berlin').localize(datetime.datetime(2017, 10, 17, 15, 45, 3)),
+        'dt_naive_to_utc': datetime.datetime(2017, 10, 17, 15, 45, 3),
+        'dt_berlin_to_utc': pytz.timezone('Europe/Berlin').localize(datetime.datetime(2017, 10, 17, 15, 45, 3)),
+        'dt_utc_to_berlin': datetime.datetime(2017, 10, 17, 15, 45, 3, tzinfo=pytz.utc),
+        'dt_skip_none': None,
+    }
+    obj = objectify(data)
+    assert MyMapper().dump(obj) == {
+        'dt_naive': '2017-10-17T15:45:03+00:00',
+        'dt_berlin': '2017-10-17T15:45:03+02:00',
+        'dt_naive_to_utc': '2017-10-17T15:45:03+00:00',
+        'dt_berlin_to_utc': '2017-10-17T13:45:03+00:00',
+        'dt_utc_to_berlin': '2017-10-17T17:45:03+02:00',
     }
 
 
@@ -67,9 +132,13 @@ def test_mapper_method_field():
         squared = lion.MapperMethodField()
         def get_squared(self, obj):
             return obj.x ** 2
-    data = {'x': 8}
+    data = {
+        'x': 8
+    }
     obj = objectify(data)
-    assert MyMapper().dump(obj) == {'squared': 64}
+    assert MyMapper().dump(obj) == {
+        'squared': 64
+    }
 
 
 def test_list_field():
@@ -87,9 +156,26 @@ def test_list_mapper_field():
         title = lion.StrField()
     class MyMapper(lion.Mapper):
         l = lion.ListField(SthMapper)
-    data = {'l': [{'title': 'foo'}, {'title': 'bar'}]}
+        l_empty = lion.ListField(SthMapper)
+        l_skip_none = lion.ListField(SthMapper, condition=lion.skip_none)
+        l_skip_empty = lion.ListField(SthMapper, condition=lion.skip_empty)
+    data = {
+        'l': [
+            {'title': 'foo'},
+            {'title': 'bar'}
+        ],
+        'l_empty': [],
+        'l_skip_none': None,
+        'l_skip_empty': [],
+    }
     obj = objectify(data)
-    assert MyMapper().dump(obj) == data
+    assert MyMapper().dump(obj) == {
+        'l': [
+            {'title': 'foo'},
+            {'title': 'bar'}
+        ],
+        'l_empty': [],
+    }
 
 
 def test_mapper_field():
