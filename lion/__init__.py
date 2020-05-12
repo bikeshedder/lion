c30319d3c9bb03e7d2606ed25b149a021531f76e
Michael P. Jung <michael.jung@terreon.de>
2020-05-12 20:38:21 +0200

    Remove __version__ string from module root
    Retrieving the version during runtime can be done using the
    `pkg_resources` module.

--- lion/__init__.py
+++ lion/__init__.py
@@ -6,5 +6,3 @@ from .update import update_object
 from .conditions import no_condition, skip_none, skip_empty, skip_false
 from . import getters
 from .ql import parse as parse_ql
-
-__version__ = '0.3.0'
